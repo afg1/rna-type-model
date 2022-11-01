a3f14bc6d03aaec87c37400d7767d18b107c4dd1
Andrew Green <agreen@ebi.ac.uk>
2022-11-01 20:41:50 +0000

    Include new LFs and split processing

--- rna_type/heuristic/run_heuristic_labeller.py
+++ rna_type/heuristic/run_heuristic_labeller.py
@@ -1,43 +1,71 @@
 #!/usr/bin/env python
 import click
 from snorkel.labeling import PandasLFApplier, LFAnalysis
+from snorkel.labeling.apply.dask import PandasParallelLFApplier
 import polars as pl
 import numpy as np
-
-from rna_type.labelling.coarse_labelling_functions import (
-    length_based,
-    score_based,
-    accession_based,
-    coverage_based,
-    taxonomy_based_r2dt,
-    taxonomy_based_rfam,
-)
-from labelling.labels import RNATypeCoarse
-
-
-lfs = [
-    accession_based,
-    score_based,
-    taxonomy_based_r2dt,
-    taxonomy_based_rfam,
-    length_based,
-    coverage_based,
-]
 
 
 @click.command()
 @click.argument("train_data")
 @click.argument("output")
-def run_heuristic_labeller(train_data, output):
-    applier = PandasLFApplier(lfs)
+@click.option("--parallel", default=False)
+def run_heuristic_labeller(train_data, output, parallel):
+    from ..labelling.coarse_labelling_functions import (
+        length_based,
+        score_based,
+        accession_based,
+        coverage_based,
+        taxonomy_based_rfam,
+        passthrough,
+        is_lncRNA,
+        is_miRNA,
+    )
 
-    # print(df.dtypes)
-    df = pl.read_parquet(train_data).to_pandas()
+    lfs = [
+        accession_based,
+        score_based,
+        taxonomy_based_rfam,
+        length_based,
+        coverage_based,
+        passthrough,
+        is_lncRNA,
+        is_miRNA,
+    ]
 
-    L_train = applier.apply(df)
+    print("Loaded label functions, preparing to apply")
+    if parallel:
+        print("Building parallel applier...")
+        applier = PandasParallelLFApplier(lfs)
+    else:
+        print("Building single-thread applier...")
+        applier = PandasLFApplier(lfs)
+
+    print("Reading parquet")
+    df = pl.read_parquet(train_data)
+    df = df.sort(pl.col("dbid").arr.lengths())
+
+    print("Starting Applier...")
+    chunk_size = 100_000_000
+    L_train = None
+    for offset in range(0, df.height):
+        print(offset)
+        df_slice = df.slice(offset * chunk_size, chunk_size)
+        L_train_slice = applier.apply(df_slice.to_pandas())
+        if offset == 0:
+            L_train = L_train_slice
+        else:
+            L_train = np.vstack((L_train, L_train_slice))
+
+    print(L_train.shape, df.height)
+
+    # L_train = applier.apply(df)
+
+    print("Done, saving output")
     np.save(output, L_train)
 
     ## Evaluate label model
+    print("Label function evaluation details: ")
     print(LFAnalysis(L=L_train, lfs=lfs).lf_summary())
 
 
