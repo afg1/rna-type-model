e554d8cb342dc35b95ea40a122012d5963fca09b
Andrew Green <agreen@ebi.ac.uk>
2022-11-02 15:18:19 +0000

    Include left join on secondary structure layout models

--- rna_type/preparation/query.sql
+++ rna_type/preparation/query.sql
@@ -16,7 +16,7 @@ from xref
 join rnc_accessions ac on xref.ac = ac.accession
 join rnc_rna_precomputed pre on pre.upi = xref.upi and pre.taxid = xref.taxid
 left join rnc_secondary_structure_layout layout on layout.urs = xref.upi
-JOIN rnc_secondary_structure_layout_models models ON models.id = layout.model_id
+LEFT JOIN rnc_secondary_structure_layout_models models ON models.id = layout.model_id
 LEFT JOIN rfam_model_hits hits on hits.upi = xref.upi
 LEFT JOIN rfam_models rfam ON rfam.rfam_model_id = hits.rfam_model_id
 where
